e4d77c17cff437f5d1a711be83dd0e8125a69ec4
Mohammed-Karimullah <mohammed03sk9@gmail.com>
2024-08-27 22:21:32 +0900

    Add files via upload

---
+++ Mini Calender/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const monthNameEl = document.getElementById("month-name");
+    const dayNameEl = document.getElementById("day-name");
+    const dayNumEl = document.getElementById("day-number");
+    const yearEl = document.getElementById("year");
+  
+    if (monthNameEl && dayNameEl && dayNumEl && yearEl) {
+      const date = new Date();
+      
+      monthNameEl.innerText = date.toLocaleString("en", { month: "long" });
+      dayNameEl.innerText = date.toLocaleString("en", { weekday: "long" });
+      dayNumEl.innerText = date.getDate();
+      yearEl.innerText = date.getFullYear();
+    } else {
+      console.error("One or more elements not found.");
+    }
+  });
